3908f22c418df3e9c3296e63a60bd8ac9dde11c2
Prakhar-187 <prakharj3105@gmail.com>
2025-01-05 18:29:38 +0530

    corrected code 26 on loops

--- Assignment_1/26.py
+++ Assignment_1/26.py
@@ -1,11 +1,8 @@
-for i in range(1,8):
-    if(i==1 or i==7):
-        print("****")
-    elif(i==2 or i==3):
-        print("*")
-    elif(i==4):
-        print(" ",end="")
-        print("***")
-    else:
-        print("  ",end="")
-        print("*")
+for i in range (0,7):
+    for j in range (0,5):
+        if(i==0 or i==6 or ((0<i<4) and j==0) or ((3<i<6) and j==4) or (i==3)):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print("")
+    
